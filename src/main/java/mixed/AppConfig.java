46357fe2be7e2f8a211b4752203b65199062f1ab
gitBelep <ddawid3@freemail.hu>
2021-08-01 23:29:40 +0200

    @PropertySource

--- src/main/java/mixed/AppConfig.java
+++ src/main/java/mixed/AppConfig.java
@@ -1,11 +1,25 @@
 package mixed;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @ComponentScan(basePackageClasses = AppConfig.class)
 @ImportResource("classpath:/appl-context-mixed.xml")
+@PropertySource("classpath:/app.properties")
 public class AppConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public String printAppVersion(){
+        String ver = environment.getProperty("version");
+        System.out.println(ver);
+        System.out.println(environment.getProperty("OS") + ": type of the OP system.");
+        return ver;
+    }
 
     // @Autowired
     //    private Environment environment;
